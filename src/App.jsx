3393387a53abe3c5d5e74f326ac9f3346db533e9
shagufta333 <shagufta.batool8@gamil.com>
2023-02-23 22:50:07 +0100

    formats app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,21 @@
 import React from "react";
-import Header from "./components/header";
 import "./App.css";
-import Welcome  from "./components/welcome";
+import Header from "./components/header";
+import Welcome from "./components/welcome";
 import About from "./components/about";
 import Contact from "./components/contact";
-import { Routes, Route  } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <>
-      {/* <div className="container">//</div> */}
-        <Header />
-        <Routes>
+      <Header />
+      <Routes>
         <Route exact path="/" component={Welcome}></Route>
         <Route exact path="/about" component={About}></Route>
-        
         <Route exact path="/contact" component={Contact}></Route>
-        </Routes>
+      </Routes>
 
-        <Welcome />
-      
-        
-      
-
-      {/* <BrowserRouter>
-  
-     
-     <Route exact path="/" component={Header} />
-
-  
-  
-  
-  </BrowserRouter> */}
+      <Welcome />
     </>
   );
 };
