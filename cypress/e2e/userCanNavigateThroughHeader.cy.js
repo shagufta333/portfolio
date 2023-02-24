ddf0cdac97a9dc3224bd8a3e25cd6c595082dd97
shagufta333 <shagufta.batool8@gamil.com>
2023-02-24 23:29:25 +0100

    all tests are passing in header section

--- cypress/e2e/userCanNavigateThroughHeader.cy.js
+++ cypress/e2e/userCanNavigateThroughHeader.cy.js
@@ -7,7 +7,7 @@ describe("user can navigate through links in header", () => {
     beforeEach(() => {
       cy.get("[data-cy=about-tab]").click();
     });
-    it("displays About Me Tab", () => {
+    it("displays About Me tab", () => {
       cy.get("[data-cy=about-tab]").should("contain", "About Me");
     });
     it("displays component name in url", () => {
@@ -18,22 +18,33 @@ describe("user can navigate through links in header", () => {
     beforeEach(() => {
       cy.get("[data-cy=portfolio-tab]").click();
     });
-    it("displays My Portfolio Tab", () => {
+    it("displays My Portfolio tab", () => {
       cy.get("[data-cy=portfolio-tab]").should("contain", "My Portfolio");
     });
     it("displays component name in url", () => {
       cy.url().should("contain", "/");
     });
   });
-  context.only("MyProjects-tab Section", () => {
+  context("MyProjects-tab Section", () => {
     beforeEach(() => {
       cy.get("[data-cy=projects-tab]").click();
     });
-    it("displays My Projects Tab", () => {
+    it("displays My Projects tab", () => {
       cy.get("[data-cy=projects-tab]").should("contain", "My Projects");
     });
     it("displays component name in url", () => {
       cy.url().should("contain", "/myProjects");
+    });
+  });
+  context("Contact-tab Section", () => {
+    beforeEach(() => {
+      cy.get("[data-cy=contact-tab]").click();
+    });
+    it("displays Contact tab", () => {
+      cy.get("[data-cy=contact-tab]").should("contain", "Contact");
+    });
+    it("displays component name in url", () => {
+      cy.url().should("contain", "/contact");
     });
   });
 });
