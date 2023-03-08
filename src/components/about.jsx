8459c3f778c2d24138d399d2535a95b7bba3af33
shagufta333 <shagufta.batool8@gamil.com>
2023-03-08 20:08:46 +0100

    add react-reveak fade to About component

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
+import Fade from "react-reveal/Fade";
 
-const About = () =>{
+const About = () => {
+  return (
+    <Container text>
+      <Fade top>
+          <h1 id="about-header">About Me</h1>
+        </Fade>
+        <Fade right big>
+          <p>
+            With a background in computer science, serving mostly as a boat captain
+            and working in sales, the time has come to start my career as developer. My passion
+            for <code>coding</code>, technology and problem solving lead me to
+            career in IT. My prefered process is{" "}
+            <strong>Test Driven Development</strong> and adapting the{" "}
+            <strong>Agile Methodology</strong> of software development.
+          </p>
+        </Fade>
+    </Container>
+  );
+};
 
-  return(
-<h1>about me</h1>
-  )
-}
-
-export default About
+export default About;
