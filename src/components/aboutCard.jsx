8491e0d9a7bee4106e3dbe7405dad545e05296bd
shagufta333 <shagufta.batool8@gamil.com>
2023-03-18 21:21:57 +0100

    formats and fixes about card component

--- src/components/aboutCard.jsx
+++ src/components/aboutCard.jsx
@@ -5,21 +5,24 @@ const AboutCard = ({ about }) => {
   return (
     <div class="popup">
       <div class="popup-wrapper">
-      <Item.Group>
-        <Item>
-          <Item.Content>
-            <Item.Header as="h3">Name: {about.name}</Item.Header>
-           
-            <Item.Extra>Location:{about.location}</Item.Extra>
-            
-            <Item.Description  style={{ color: 'gray' }}>Intrerest: {about.hobbies}</Item.Description> 
-            <Item.Extra>Languages:{about.lang}</Item.Extra> 
-            <Item.Description  style={{ color: 'gray' }}>Keywords: {about.keyw}</Item.Description> 
-          
-          </Item.Content>
-        </Item>
-      </Item.Group>
-    </div>
+        <Item.Group>
+          <Item>
+            <Item.Content>
+              <Item.Header as="h3">Name: {about.name}</Item.Header>
+
+              <Item.Extra>Location:{about.location}</Item.Extra>
+
+              <Item.Description style={{ color: "gray" }}>
+                Intrerest: {about.hobbies}
+              </Item.Description>
+              <Item.Extra>Languages:{about.lang}</Item.Extra>
+              <Item.Description style={{ color: "gray" }}>
+                Keywords: {about.keyw}
+              </Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </div>
     </div>
   );
 };
