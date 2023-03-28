faeca0f5ac3aee60c1724f27cba4ff0340d77794
shagufta333 <shagufta.batool8@gamil.com>
2023-03-28 21:46:56 +0200

    first two tests passes for about component

--- cypress/e2e/userCanSeeDetailsOfAboutPage.cy.js
+++ cypress/e2e/userCanSeeDetailsOfAboutPage.cy.js
@@ -1,29 +1,18 @@
 describe("User can see details of about page", () => {
   beforeEach(() => {
-    // cy.intercept("GET", "**/about", {
-    //   fixture: "about.json",
-    // }).as("getAbout");
-
+    cy.intercept("GET", "**/about", {
+      fixture: "about.json",
+    }).as("getAbout");
     cy.visit("/");
-  
-   
-  });
-//   beforeEach(() => {
-//     cy.visit("/");
-//     
-//   });
-context("",()=>{
-  beforeEach(() => {
-    cy.get("[data-cy=about-tab]").click({ force: true });
-  });
-  it("displays a popup with bio info", () => {
-    //cy.get("[data-cy=about-tab]").click();
+    cy.get("[data-cy=about-tab]").click();
     cy.get("[data-cy=about-header]").trigger("mouseover");
-    // cy.get(".ui.left.center.popup.transition.visible").should("exist");
-//     cy.get(".ui.left.center.popup.transition.visible").within(() => {
-//       cy.get(".header").should("exist");
-//     });
-   });
-})
-   
- });
+    cy.get(".ui.left.center.popup.transition.visible").should("exist");
+  });
+
+  it("displays a popup with bio info", () => {
+    cy.get(".ui.left.center.popup.transition.visible").within(() => {
+      cy.get(".header").should("exist");
+      cy.get(".header").should("contain", "Shagufta Batool");
+    });
+  });
+});
